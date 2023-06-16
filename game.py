36fb4cb6a67f5676ad69efd6b9d54a04fcf78369
soukasamadi <>
2023-06-16 23:37:13 +0000

    Display_user_choices to ask the player  to pick a number

--- game.py
+++ game.py
@@ -13,4 +13,9 @@ class Game:
         self.rules = Rules()
         self.computer: str = "computer"
 
-        
+
+
+    def Display_user_choices(self) -> None:
+       
+        choices_text = "\n ".join(f"{entity.value} : {entity.name}" for entity  in self.entities)
+        print(f"Select a number[1-2-3-4-5]:\n {choices_text}:", end='\t')    
