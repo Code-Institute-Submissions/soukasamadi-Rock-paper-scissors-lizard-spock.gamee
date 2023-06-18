988e2c5018a7b718e4378f5655aef0ac1b7fe6bb
soukasamadi <>
2023-06-18 16:51:09 +0000

    Add ASCII Logo

--- run.py
+++ run.py
@@ -5,9 +5,41 @@ from colorama import Fore
 def main() -> None:
     """Main function to start the game
     """
+    
+    logo = [
+    # Game logo
+    """
+    Welcome To :                      
+                
+    ______                 ______                             
+    | ___ \         | |    | ___ \                            
+    | |_/ /___   ___| | __ | |_/ /_ _ _ __   ___ _ __         
+    |    // _ \ / __| |/ / |  __/ _` | '_ \ / _ \ '__|        
+    | |\ \ (_) | (__|   <  | | | (_| | |_) |  __/ |           
+    \_| \_\___/ \___|_|\_\ \_|  \__,_| .__/ \___|_|           
+    _____ _                        | |                      
+    /  ___(_)                       |_|                      
+    \ `--. _ ___ ___  ___  _ __ ___                          
+    `--. \ / __/ __|/ _ \| '__/ __|                         
+    /\__/ / \__ \__ \ (_) | |  \__ \                         
+    \____/|_|___/___/\___/|_|  |___/                         
+    _     _                  _   _____                  _    
+    | |   (_)                | | /  ___|                | |   
+    | |    _ ______ _ _ __ __| | \ `--. _ __   ___   ___| | __
+    | |   | |_  / _` | '__/ _` |  `--. \ '_ \ / _ \ / __| |/ /
+    | |___| |/ / (_| | | | (_| | /\__/ / |_) | (_) | (__|   < 
+    \_____/_/___\__,_|_|  \__,_| \____/| .__/ \___/ \___|_|\_\
+                                                        
+                                    |_|                                      
+    """
+    ]
+
+
+    print(f"{Fore.BLUE}{logo[0]}")
+
     user_name = Game.get_user_name()
     game = Game(user_name)
-    print(Fore.BLUE + '  --- Welcome To Rock paper scissor spock and lizard game! ---\n')  
+    
     print(Fore.YELLOW + '       Game Rules:\n')
     print(Fore.YELLOW + ' -Scissors decapitate Lizard.\n -Scissors cuts paper.\n -Paper covers rock.\n -Rock crushes lizard.\n -Lizard poisons Spock.\n -Spock smashes scissors.\n -Scissors decapitates lizard.\n -Lizard eats paper.\n -Paper disproves Spock.\n -Spock vaporizes rock.\n -Rock crushes scissors.\n')
     print(Fore.BLUE + '------You will play 5 Rounds------\n')
